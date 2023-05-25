8d3466dcac2a89ec28958061f904f75510bc693b
kalknord <133772407+kalknord@users.noreply.github.com>
2023-05-25 11:40:19 +0200

    prog

---
+++ Mitarbeitende/Mitarbeitende.java
@@ -0,0 +1,26 @@
+public class Mitarbeitende {
+    private String name;
+    private char gender;
+    private String birthdate;
+
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public String getBirthdate() {
+        return birthdate;
+    }
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+    
+}
