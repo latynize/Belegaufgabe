f1b4d0fe96db9f5a9cbcd5172dfea9c73b44a84d
latynize <133243898+latynize@users.noreply.github.com>
2023-05-29 19:44:10 +0200

    test

--- HCM/academic.java
+++ HCM/academic.java
@@ -3,4 +3,5 @@ public class academic extends employees implements salarygroup {
     public academic(String name, char gender, String birthdate) {
         super(name, gender, birthdate);
     }
+    //test 
 }
